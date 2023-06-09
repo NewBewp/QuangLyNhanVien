35a2b973dfd92216fbb2a2ab1152b3501182f1fe
Ha Kien <nguyenhakien99@gmail.com>
2023-06-09 21:41:57 +0700

    chua xong

--- controler/main.js
+++ controler/main.js
@@ -1,5 +1,15 @@
+function getElement(selector) {
+    return document.querySelector(selector);
+}
+
 var arrNV = [];
 
 function addNV() {
-    
+    var tknv = getElement("#getElement").value;
+    var name = getElement("#name").value;
+    var email = getElement("#email").value;
+    var password = getElement("#password").value;
+    var datepicker = getElement("#datepicker").value;
+
 }
+getElement("btnThemNV").onclick = addNV;
